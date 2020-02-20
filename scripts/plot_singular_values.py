cc14c3570353fe896873a3f913d06d244a944821
phueb <info@philhuebner.com>
2020-02-20 11:58:51 -0600

    rename

--- scripts/plot_singular_values.py
+++ scripts/plot_singular_values.py
@@ -9,30 +9,30 @@ from straddler.figs import make_example_fig
 from straddler.toy_corpus import ToyCorpus
 from straddler.figs import plot_singular_values
 
-DOC_SIZE = 1_000_000
-NUM_TYPES = 1024  # this needs to be large to result in reduction in conditional entropy
+DOC_SIZE = 100_000
+NUM_TYPES = 1024  # this needs to be large to provide room for interesting effects
 NUM_XWS = 512
-NUM_FRAGMENTS = 3  # can be used in combination with FRAGMENTED_CONTROl
-FRAGMENTATION_PROB = 1.0  # TODO test
+NUM_FRAGMENTS = 3  # number of x-word sub categories, or singular dimensions
+FRAGMENTATION_PROB = 0.8  # the higher, the more prominent are sub-categories (and singular values)
+NUM_SINGULAR_DIMS_PLOT = 8
 
 
-tc = ToyCorpus(doc_size=DOC_SIZE,
-               num_types=NUM_TYPES,
-               num_xws=NUM_XWS,
-               num_fragments=NUM_FRAGMENTS,
-               fragmentation_prob=FRAGMENTATION_PROB,
-               )
-prep = PartitionedPrep([tc.doc],
+toy_corpus = ToyCorpus(doc_size=DOC_SIZE,
+                       num_types=NUM_TYPES,
+                       num_xws=NUM_XWS,
+                       num_fragments=NUM_FRAGMENTS,
+                       fragmentation_prob=FRAGMENTATION_PROB,
+                       )
+prep = PartitionedPrep([toy_corpus.doc],
                        reverse=False,
                        num_types=NUM_TYPES,
                        num_parts=2,
                        num_iterations=[1, 1],
                        batch_size=64,
                        context_size=1)
-probes = [p for p in tc.xws if p in prep.store.w2id]
+probes = [p for p in toy_corpus.xws if p in prep.store.w2id]
 
 s_list = []
-num_sv = 8
 for part_id, part in enumerate(prep.reordered_parts):
 
     # windows
@@ -46,11 +46,11 @@ for part_id, part in enumerate(prep.reordered_parts):
     probe_windows = windows[row_ids]
 
     # conditional entropy
-    x = probe_windows[:, -2]  # CAT member
-    y = probe_windows[:, -1]  # next-word
+    x = probe_windows[:, -2]  # x-word
+    y = probe_windows[:, -1]  # y-word
     x_y = np.vstack((x, y))   # joint outcome
 
-    # map word ID of nouns to IDs between [0, len(probes)]
+    # map word ID of x-words to IDs between [0, len(probes)]
     # this makes creating a matrix with the right number of columns easier
     x2x = {xi: n for n, xi in enumerate(np.unique(x))}
 
@@ -58,22 +58,17 @@ for part_id, part in enumerate(prep.reordered_parts):
     cf_mat = np.ones((prep.num_types, len(x2x))) * 1e-9
     for xi, yi in zip(x, y):
         cf_mat[yi, x2x[xi]] += 1
-    last_num_rows = NUM_TYPES - NUM_XWS  # other rows are just empty because of nouns not occurring with nouns
+    last_num_rows = NUM_TYPES - NUM_XWS  # other rows are just empty because of x-words not occurring with x-words
     fig, ax = make_example_fig(np.log(cf_mat[-last_num_rows:]))
     ce = drv.entropy_conditional(x, y).item()
     je = drv.entropy_joint(x_y).item()
     ye = drv.entropy_joint(y).item()
-    plt.title(f'Toy Corpus Part {part_id+1}\nH(noun|slot 2)={ce:.4f}\nH(noun,slot 2)={je:.4f}\nH(slot 2)={ye:.4f}')
+    plt.title(f'Toy Corpus Part {part_id+1}\nH(x-word|y-word)={ce:.4f}\nH(x-word,y-word)={je:.4f}\nH(y-word)={ye:.4f}')
     plt.show()
 
-    print(np.sum(np.var(cf_mat, axis=0)))  # row-wise variance is higher for part 1
-    print(np.sum(np.var(cf_mat, axis=1)))
-    print(np.sum(cf_mat))
-
-    # SVD
+    # collect singular values for plotting
     s = np.linalg.svd(cf_mat, compute_uv=False)
-    print(f'first {num_sv} singular values', ' '.join(['{:>6.2f}'.format(si) for si in s[:num_sv]]))
-    s_list.append(np.asarray(s[:num_sv]))
+    s_list.append(np.asarray(s[:NUM_SINGULAR_DIMS_PLOT]))
 
-plot_singular_values(s_list, max_s=num_sv)
+plot_singular_values(s_list, max_s=NUM_SINGULAR_DIMS_PLOT)
 
--- straddler/toy_corpus.py
+++ straddler/toy_corpus.py
@@ -1,4 +1,3 @@
-from typing import List
 from cached_property import cached_property
 import random
 from itertools import cycle
@@ -30,13 +29,9 @@ class ToyCorpus:
         # a smaller set of yws
         self.yws_limited = [o for o in self.yws if float(o[1:]) % num_fragments == 0]
 
-        # subsets of xws precede subsets of yws
+        # map subsets of xws to mutually exclusive subsets of yws
         c = cycle([self.yws[offset::num_fragments] for offset in range(num_fragments)])
         self.xw2yws = {xw: next(c) for xw in self.xws}
-
-        for k, v in self.xw2yws.items():
-            print(k)
-            print(v)
 
         print('Initialized ToyCorpus with number of limited yws:', len(self.yws_limited))
 
@@ -48,10 +43,10 @@ class ToyCorpus:
             # sample xw randomly
             xw = random.choice(self.xws)
 
-            # sample next-word from one of many sub-population (this keeps overall type frequency the same)
+            # sample yw from one of many sub-population (this keeps overall type frequency the same)
             if random.random() < self.fragmentation_prob:
                 yw = random.choice(self.xw2yws[xw])
-            # sample next-word from a single sub-population
+            # sample yw from a single sub-population
             else:
                 yw = random.choice(self.yws_limited)
             res += f'{xw} {yw} '  # whitespace after each
