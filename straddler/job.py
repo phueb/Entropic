944b27cc663b19c89b6a67251dec716292ae05e5
phueb <info@philhuebner.com>
2020-02-20 08:43:33 -0600

    remove old eval fn

--- straddler/job.py
+++ straddler/job.py
@@ -44,18 +44,14 @@ def main(param2val):
     net = Net(params, data)
 
     # optimizer + criterion
-    # noinspection PyUnresolvedReferences
     optimizer = optim.SGD(net.parameters(), lr=params.lr)
-    criterion = torch.nn.MSELoss()
+    criterion = torch.nn.MSELoss()  # TODO cross entropy
 
     # eval before start of training
-    # noinspection PyUnresolvedReferences
     net.eval()
     torch_o = net(data.torch_x)
     torch_y = torch.from_numpy(data.make_y(epoch=0))
     loss = criterion(torch_o, torch_y)
-    # noinspection PyUnresolvedReferences
-    net.train()
 
     # train loop
     eval_epoch_idx = 0
@@ -63,63 +59,38 @@ def main(param2val):
     scores_b = np.zeros(config.Eval.num_evals)
     eval_epochs = to_eval_epochs(params)
     for epoch in range(params.num_epochs + 1):
+
         # eval
         if epoch in eval_epochs:
             # cluster score
+            net.eval()
             print('Evaluating at epoch {}'.format(epoch))
-            collect_scores(data, params, net, eval_epoch_idx, scores_a, scores_b, torch_o)
+            sim_mat = cosine_similarity(rep_mat)
+            sim_mat_gold = np.rint(cosine_similarity(rep_mat_gold))
+            score = calc_cluster_score(sim_mat, sim_mat_gold, config.Eval.metric)
             eval_epoch_idx += 1
+
             # mse
             mse = loss.detach().numpy().item()
-            print('mse={}'.format(mse))
+            print('pp={}'.format(mse), flush=True)
             print()
-            sys.stdout.flush()
 
-        y = data.make_y(epoch)
+        # get batch
+        torch_x = torch.from_numpy(x)
+        torch_y = torch.from_numpy(y)
 
         # train
+        net.train()
         optimizer.zero_grad()  # zero the gradient buffers
-        torch_o = net(data.torch_x)  # feed-forward
-        torch_y = torch.from_numpy(y)
+        torch_o = net(torch_x)  # feed-forward
         loss = criterion(torch_o, torch_y)  # compute loss
         loss.backward()  # back-propagate
         optimizer.step()  # update
 
     # return performance as pandas Series # TODO return series not df
-    eval_epochs = to_eval_epochs(params)
-    df_a = pd.DataFrame(scores_a, index=eval_epochs, columns=[config.Eval.metric])
-    df_b = pd.DataFrame(scores_b, index=eval_epochs, columns=[config.Eval.metric])
-    df_a.name = 'results_a'
-    df_b.name = 'results_b'
+    s_a = pd.Series(scores_a, index=eval_epochs)
+    s_b = pd.Series(scores_b, index=eval_epochs)
+    s_a.name = 'results_a'
+    s_b.name = 'results_b'
 
-    return df_a, df_b
-
-
-def collect_scores(data, params, net, eval_epoch_idx, scores_a, scores_b, torch_o):
-    # rep_mat
-    if params.representation == 'output':
-        rep_mat = torch_o.detach().numpy()  # TODO make gif of evolving prediction mat
-    elif params.representation == 'hidden':
-        rep_mat = net.linear1(data.torch_x).detach().numpy()
-    else:
-        raise AttributeError('Invalid arg to "representation".')
-    #
-    rep_mats = [rep_mat[:data.superordinate_size],
-                rep_mat[-data.superordinate_size:]]
-    rep_mats_gold = [data.y1_gold[:data.superordinate_size],
-                     data.y1_gold[-data.superordinate_size:]]
-    for scores, rep_mat, rep_mat_gold in zip([scores_a, scores_b], rep_mats, rep_mats_gold):
-        if scores[max(0, eval_epoch_idx - 1)] == 1.0:
-            continue
-        sim_mat = cosine_similarity(rep_mat)
-        sim_mat_gold = np.rint(cosine_similarity(rep_mat_gold))
-        score = calc_cluster_score(sim_mat, sim_mat_gold, config.Eval.metric)
-        scores[eval_epoch_idx] = score
-        #
-        if score == 1.0:
-            scores[eval_epoch_idx:] = 1.0
-        #
-        print(rep_mat.round(3))
-        print(sim_mat_gold.round(1))
-        print(sim_mat.round(4))
-        print('{}={}'.format(config.Eval.metric, score))
+    return s_a, s_b
